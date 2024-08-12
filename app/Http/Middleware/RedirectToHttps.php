d8f4d8c861de2d04c9d743bda6d1b1b7be4c47ef
melvyndev <154402936+melvyndev@users.noreply.github.com>
2024-08-12 13:07:57 +0400

    fix2

--- app/Http/Middleware/RedirectToHttps.php
+++ app/Http/Middleware/RedirectToHttps.php
@@ -4,17 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class RedirectToHttps
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+        if (!$request->secure() && !$request->is('favicon.ico')) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
         return $next($request);
     }
 }
