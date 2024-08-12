ebf82feef94f838008a299a8ea6cb7d68467b928
melvyndev <154402936+melvyndev@users.noreply.github.com>
2024-08-12 14:32:19 +0400

    acces

--- app/Http/Middleware/RedirectToHttps.php
+++ app/Http/Middleware/RedirectToHttps.php
@@ -4,17 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class RedirectToHttps
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+        if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
         return $next($request);
     }
 }
